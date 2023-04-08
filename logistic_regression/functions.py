d9e4bd3b55552c58d6dba08f3c75d4524d400dba
Philip Hierhager <philip.hierhager@googlemail.com>
2023-04-08 18:46:03 +0200

    refactor: docstrings

--- logistic_regression/functions.py
+++ logistic_regression/functions.py
@@ -103,12 +103,8 @@ def optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=Fal
     Returns:
     params -- dictionary containing the weights w and bias b
     grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
-    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    costs -- list of all the costs computed during the optimization
 
-    Tips:
-    You basically need to write down two steps and iterate through them:
-        1) Calculate the cost and the gradient for the current parameters. Use propagate().
-        2) Update the parameters using gradient descent rule for w and b.
     """
 
     w = copy.deepcopy(w)
